befc911930965ca73e41b40fe3fe76f3631b6bec
JessicaVasseur123 <86002179+JessicaVasseur123@users.noreply.github.com>
2023-02-01 22:31:14 +0100

    #37 ETL-Skript an neues Datenmodell anpassen
    - SQL Statements für Erstellung der DWH Tabellen angepasst
    - SNOMED CT Codes als INTEGER angelegt
    - Statements für Option 2 und Zwischenschritte gelöscht

--- Code/etl_sql_entwurf.sql
+++ Code/etl_sql_entwurf.sql
@@ -1,6 +1,6 @@
--- SQL statements to create tables of DWH
+-- SQL statements to create tables of DWH database
 
--- option 1: table "F_encounter_costs"
+-- table "F_encounter_costs"
 CREATE TABLE IF NOT EXISTS F_encounter_costs
              (
                 Id                      VARCHAR PRIMARY KEY,
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS F_encounter_costs
                 PRO_DATE                DATETIME,
                 PRO_CODE                INTEGER,
                 PRO_BASE_COST           FLOAT,
-                PRO_REASON              VARCHAR,
+                PRO_REASON              INTEGER,
                 MED_START               DATETIME,
                 MED_STOP                DATETIME,
                 MED_CODE                INTEGER,
@@ -24,15 +24,15 @@ CREATE TABLE IF NOT EXISTS F_encounter_costs
                 MED_TOTAL_COST          FLOAT,
                 MED_PAYER_COVERAGE      FLOAT,
                 MED_PAYER               VARCHAR,
-                MED_REASON              VARCHAR,
+                MED_REASON              INTEGER,
                 CON_START               DATETIME,
                 CON_STOP                DATETIME,
                 CON_CODE                INTEGER,
-                FOREIGN KEY (PATIENT)   
+                FOREIGN KEY (PATIENT)
                     REFERENCES D_patients (Id),
-                FOREIGN KEY (ENC_PAYER)     
+                FOREIGN KEY (ENC_PAYER)
                     REFERENCES D_payers (Id),
-                FOREIGN KEY (MED_PAYER) 
+                FOREIGN KEY (MED_PAYER)
                     REFERENCES D_payers (Id),
                 FOREIGN KEY (ENC_REASON)
                     REFERENCES D_snomedct (Code),
@@ -46,69 +46,7 @@ CREATE TABLE IF NOT EXISTS F_encounter_costs
                     REFERENCES D_snomedct (Code),
                 FOREIGN KEY (MED_CODE)
                     REFERENCES D_rxnorm (Code)
-             )
-
--- option 2: table "F_encounter_costs"
-CREATE TABLE IF NOT EXISTS F_encounter_costs
-             (
-                Id                      VARCHAR PRIMARY KEY,
-                ENC_START               DATETIME,
-                ENC_STOP                DATETIME,
-                PATIENT                 VARCHAR,
-                ENC_PAYER               VARCHAR,
-                ENC_BASE_COST           FLOAT,
-                ENC_TOTAL_CLAIM_COST    FLOAT,
-                ENC_PAYER_COVERAGE      FLOAT,
-                ENC_REASON              INTEGER,
-                PRO_DATE                DATETIME,
-                PRO_CODE                INTEGER,
-                PRO_BASE_COST           FLOAT,
-                PRO_REASON              VARCHAR,
-                MED_START               DATETIME,
-                MED_STOP                DATETIME,
-                MED_CODE                INTEGER,
-                MED_BASE_COST           FLOAT,
-                MED_DISPENSES           INTEGER,
-                MED_TOTAL_COST          FLOAT,
-                MED_PAYER_COVERAGE      FLOAT,
-                MED_PAYER               VARCHAR,
-                MED_REASON              VARCHAR,
-                FOREIGN KEY (PATIENT)   
-                    REFERENCES D_patients (Id),
-                FOREIGN KEY (ENC_PAYER)     
-                    REFERENCES D_payers (Id),
-                FOREIGN KEY (MED_PAYER) 
-                    REFERENCES D_payers (Id),
-                FOREIGN KEY (ENC_REASON)
-                    REFERENCES D_snomedct (Code),
-                FOREIGN KEY (PRO_CODE)
-                    REFERENCES D_snomedct (Code),
-                FOREIGN KEY (PRO_REASON)
-                    REFERENCES D_snomedct (Code),
-                FOREIGN KEY (MED_REASON)
-                    REFERENCES D_snomedct (Code),
-                FOREIGN KEY (MED_CODE)
-                    REFERENCES D_rxnorm (Code)
-             )
-
--- option 2: table "F_encounter_diagnoses"
-CREATE TABLE IF NOT EXISTS F_encounter_diagnoses
-             (
-                Id                      VARCHAR PRIMARY KEY,
-                ENC_START               DATETIME,
-                ENC_STOP                DATETIME,
-                PATIENT                 VARCHAR,
-                ENC_REASON              INTEGER,
-                CON_START               DATETIME,
-                CON_STOP                DATETIME,
-                CON_CODE                INTEGER,
-                FOREIGN KEY (PATIENT)   
-                    REFERENCES D_patients (Id),
-                FOREIGN KEY (ENC_REASON)
-                    REFERENCES D_snomedct (Code),
-                FOREIGN KEY (CON_CODE)
-                    REFERENCES D_snomedct (Code)
-             )
+             );
 
 -- table D_patients
 CREATE TABLE IF NOT EXISTS D_patients
@@ -123,95 +61,37 @@ CREATE TABLE IF NOT EXISTS D_patients
                 GENDER              CHAR(1),
                 HEALTHCARE_EXPENSES FLOAT,
                 HEALTHCARE_COVERAGE FLOAT
-            ) 
+            );
 
 -- table D_payers
 CREATE TABLE IF NOT EXISTS D_payers
              (
                 Id                  VARCHAR PRIMARY KEY,
                 NAME                VARCHAR
-            ) 
+            );
 
 -- table D_snomedct
 CREATE TABLE IF NOT EXISTS D_snomedct
              (
-                CODE                VARCHAR PRIMARY KEY,
+                CODE                INTEGER PRIMARY KEY,
                 TERM                VARCHAR
-            ) 
+            );
 
 -- table D_rxnorm
 CREATE TABLE IF NOT EXISTS D_rxnorm
              (
-                CODE                VARCHAR PRIMARY KEY,
+                CODE                INTEGER PRIMARY KEY,
                 NAME                VARCHAR
-            ) 
+            );
 
--- stepwise development of SQL statements to fill DWH DB
+-- SQL statements to fill DWH database with data from source database
 
--- option 1 and 2: table "F_encounter_costs"
+-- table "F_encounter_costs"
+-- enc = encounters, pro = procedures, med = medications, con = conditions
 -- select required columns from encounters
-SELECT enc.id                  AS Id,
-       enc.start               AS ENC_START,
-       enc.stop                AS ENC_STOP,
-       enc.patient             AS PATIENT,
-       enc.payer               AS ENC_PAYER,
-       enc.base_encounter_cost AS ENC_BASE_COST,
-       enc.total_claim_cost    AS ENC_TOTAL_CLAIM_COST,
-       enc.payer_coverage      AS ENC_PAYER_COVERAGE,
-       enc.reasoncode          AS ENC_REASON
-FROM   encounters AS enc
-ORDER  BY enc.id 
-
 -- join with procedures (left join on encounter id to preserve all encounters, even without procedures), selecting required columns
-SELECT enc.id                  AS Id,
-       enc.start               AS ENC_START,
-       enc.stop                AS ENC_STOP,
-       enc.patient             AS PATIENT,
-       enc.payer               AS ENC_PAYER,
-       enc.base_encounter_cost AS ENC_BASE_COST,
-       enc.total_claim_cost    AS ENC_TOTAL_CLAIM_COST,
-       enc.payer_coverage      AS ENC_PAYER_COVERAGE,
-       enc.reasoncode          AS ENC_REASON,
-       pro.date                AS PRO_DATE,
-       pro.code                AS PRO_CODE,
-       pro.base_cost           AS PRO_BASE_COST,
-       pro.reasoncode          AS PRO_REASON
-FROM   encounters AS enc
-       LEFT JOIN procedures AS pro
-              ON enc.id = pro.encounter 
-ORDER  BY enc.id 
-
 -- next join with medications (left join on encounter id to preserve all encounters, even without medications), selecting required columns
-SELECT enc.id                  AS Id,
-       enc.start               AS ENC_START,
-       enc.stop                AS ENC_STOP,
-       enc.patient             AS PATIENT,
-       enc.payer               AS ENC_PAYER,
-       enc.base_encounter_cost AS ENC_BASE_COST,
-       enc.total_claim_cost    AS ENC_TOTAL_CLAIM_COST,
-       enc.payer_coverage      AS ENC_PAYER_COVERAGE,
-       enc.reasoncode          AS ENC_REASON,
-       pro.date                AS PRO_DATE,
-       pro.code                AS PRO_CODE,
-       pro.base_cost           AS PRO_BASE_COST,
-       pro.reasoncode          AS PRO_REASON,
-       med.start               AS MED_START,
-       med.stop                AS MED_STOP,
-       med.code                AS MED_CODE,
-       med.base_cost           AS MED_BASE_COST,
-       med.dispenses           AS MED_DISPENSES,
-       med.total_cost          AS MED_TOTAL_COST,
-       med.payer_coverage      AS MED_PAYER_COVERAGE,
-       med.payer               AS MED_PAYER,
-       med.reasoncode          AS MED_REASON
-FROM   encounters AS enc
-       LEFT JOIN procedures AS pro
-              ON enc.id = pro.encounter
-       LEFT JOIN medications AS med
-              ON enc.id = med.encounter 
-ORDER  BY enc.id 
-
--- for option 1: next join with conditions (left join on encounter id to preserve all encounters, even without conditions), selecting required columns
+-- next join with conditions (left join on encounter id to preserve all encounters, even without conditions), selecting required columns
 SELECT enc.id                  AS Id,
        enc.start               AS ENC_START,
        enc.stop                AS ENC_STOP,
@@ -244,23 +124,7 @@ FROM   encounters AS enc
               ON enc.id = med.encounter
        LEFT JOIN conditions AS con
               ON enc.id = con.encounter
-ORDER  BY enc.id 
-
--- option 2: table "F_encounter_diagnoses"
--- join encounters with conditions (inner join), selecting required columns
-SELECT enc.id         AS Id,
-       enc.start      AS ENC_START,
-       enc.stop       AS ENC_STOP,
-       enc.patient    AS PATIENT,
-       enc.reasoncode AS ENC_REASON,
-       con.start      AS CON_START,
-       con.stop       AS CON_STOP,
-       con.patient    AS CON_PATIENT,
-       con.code       AS CON_CODE
-FROM   encounters AS enc
-       JOIN conditions AS con
-         ON enc.id = con.encounter
-ORDER  BY enc.id 
+ORDER  BY enc.id;
 
 -- table "D_patients"
 SELECT id,
@@ -273,14 +137,16 @@ SELECT id,
        gender,
        healthcare_expenses,
        healthcare_coverage
-FROM   patients 
+FROM   patients;
 
 -- table "D_payers"
 SELECT id,
        name
-FROM   payers 
+FROM   payers;
 
 -- table "D_snomedct"
+-- create table from codes + descriptions in source database tables
+-- would be created directly from SNOMED CT for a proper data warehouse
 SELECT pro.code        AS CODE,
        pro.description AS TERM
 FROM   procedures AS pro
@@ -300,10 +166,12 @@ UNION
 SELECT con.code        AS CODE,
        con.description AS TERM
 FROM   conditions AS con
-ORDER  BY code 
+ORDER  BY code;
 
 -- table "D_rxnorm"
+-- create table from codes + descriptions in source database tables
+-- would be created directly from RXNorm for a proper data warehouse
 SELECT DISTINCT( med.code )    AS CODE,
                med.description AS NAME
 FROM   medications AS med
-ORDER  BY code 
+ORDER  BY code;
